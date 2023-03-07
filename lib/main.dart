500b14503a11e39046b3cf191bfb8285e3911597
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-07 07:25:23 -0300

    feat: Create sketch of PerguntasApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Container());
+void main() => runApp(PerguntasApp());
+
+class PerguntasApp extends StatelessWidget {
+  const PerguntasApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container()
+    );
+  }
 }
