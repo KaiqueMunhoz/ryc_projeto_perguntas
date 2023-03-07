f631399d02e5abbc58b5c1f11fb1dfc3a676d796
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-07 07:27:40 -0300

    feat: Remove constructor

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 void main() => runApp(PerguntasApp());
 
 class PerguntasApp extends StatelessWidget {
-  const PerguntasApp({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
