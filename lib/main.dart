3c3bc7044487ba9f4eaf6ec0b46b412e1893ac51
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-07 07:28:31 -0300

    feat: Create final perguntas

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(PerguntasApp());
 
 class PerguntasApp extends StatelessWidget {
+  final perguntas = [
+    'Qual é a sua cor favorita?',
+    'Qual é o seu animal favorito?'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
