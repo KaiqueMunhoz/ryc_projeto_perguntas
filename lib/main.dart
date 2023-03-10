ccbe9d51eb5f0d2ec9f545926c71fbcfdf4b58c7
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-10 08:00:44 -0300

    feat: Change parameter name nota to pontuacao

--- lib/main.dart
+++ lib/main.dart
@@ -16,28 +16,28 @@ class _PerguntasAppState extends State<PerguntasApp> {
     {
       'texto': 'Qual é a sua cor favorita?',
       'respostas': [
-        {'texto': 'Preto', 'nota': 10},
-        {'texto': 'Vermelho', 'nota': 5},
-        {'texto': 'Verde', 'nota': 3},
-        {'texto': 'Branco', 'nota': 1},
+        {'texto': 'Preto', 'pontuacao': 10},
+        {'texto': 'Vermelho', 'pontuacao': 5},
+        {'texto': 'Verde', 'pontuacao': 3},
+        {'texto': 'Branco', 'pontuacao': 1},
       ],
     },
     {
       'texto': 'Qual é o seu animal favorito?',
       'respostas': [
-        {'texto': 'Coelho', 'nota': 10},
-        {'texto': 'Cobra', 'nota': 5},
-        {'texto': 'Elefante', 'nota': 3},
-        {'texto': 'Leão', 'nota': 1},
+        {'texto': 'Coelho', 'pontuacao': 10},
+        {'texto': 'Cobra', 'pontuacao': 5},
+        {'texto': 'Elefante', 'pontuacao': 3},
+        {'texto': 'Leão', 'pontuacao': 1},
       ],
     },
     {
       'texto': 'Qual é a seu instrutor favorito?',
       'respostas': [
-        {'texto': 'Leo', 'nota': 10},
-        {'texto': 'Maria', 'nota': 5},
-        {'texto': 'João', 'nota': 3},
-        {'texto': 'Pedro', 'nota': 1},
+        {'texto': 'Leo', 'pontuacao': 10},
+        {'texto': 'Maria', 'pontuacao': 5},
+        {'texto': 'João', 'pontuacao': 3},
+        {'texto': 'Pedro', 'pontuacao': 1},
       ],
     },
   ];
