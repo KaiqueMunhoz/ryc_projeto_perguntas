ad0f3d9d62460ec69ebf28163db252fd5407df3e
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-10 06:57:25 -0300

    feat: Change perguntas to be a List<Map<String, Object>>

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,19 @@ class PerguntasApp extends StatefulWidget {
 }
 
 class _PerguntasAppState extends State<PerguntasApp> {
-  final perguntas = [
-    'Qual é a sua cor favorita?',
-    'Qual é o seu animal favorito?'
+  final List<Map<String, dynamic>> perguntas = [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+    },
+    {
+      'texto': 'Qual é a seu instrutor favorito?',
+      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+    },
   ];
   int _perguntaSelecionada = 0;
 
@@ -25,7 +35,7 @@ class _PerguntasAppState extends State<PerguntasApp> {
         ),
         body: Column(
           children: [
-            Questao(perguntas[_perguntaSelecionada]),
+            Questao(''),
             Resposta('Resposta 1', responder),
             Resposta('Resposta 2', responder),
             Resposta('Resposta 3', responder),
