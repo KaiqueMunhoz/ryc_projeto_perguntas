fca20c6567ebb9de8780203edde29d6c3894f864
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-10 06:47:13 -0300

    feat: Add style to Resposta

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -7,9 +7,14 @@ class Resposta extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      child: Text(texto),
-      onPressed: null,
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        textColor: Colors.white,
+        color: Colors.blue,
+        child: Text(texto),
+        onPressed: null,
+      ),
     );
   }
 }
